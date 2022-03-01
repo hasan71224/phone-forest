9f38a83b82d5f93b7d72e5ee2367db03aeb45ae2
Md. Mahmudul Hasan <mdhasan71224@gmail.com>
2022-03-01 15:36:40 +0600

    add sensor

--- js/mobile-forest.js
+++ js/mobile-forest.js
@@ -1,4 +1,5 @@
-const searchPhone = () =>{
+document.getElementById('error-massage').style.display = 'none';
+const searchPhone = async () =>{
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
     // console.log(searchText);
@@ -6,29 +7,32 @@ const searchPhone = () =>{
     searchField.value = " ";
 
     // call api
-    // if(searchText == 0){
-
-    // }
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    if(searchText == 0){
+        console.log("write valueable text");
+    }
+    else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch (url)
-    .then (response => response.json())
-    .then (data => displayPhones(data.data))  
+    .then(res => res.json())
+    .then(data => displayPhones(data.data)) 
+    }  
 }
 //show in display
 const displayPhones = data =>{
-    const searchResult = document.getElementById("search-result");
+    const searchResult = document.getElementById('search-result');
     searchResult.textContent=" ";
     data.forEach(phoneData => {
+        console.log(phoneData);
         const div = document.createElement('div');
         div.classList.add('col');
         // create div and new Element...................
         div.innerHTML = `
-            <div class="card h-100">
+            <div onclick="loadPhoneDetails(${phoneData.slug})" class="card h-100">
                 <img src="${phoneData.image}" class="card-img-top" alt="image">
                 <div class="card-body">
                   <h5 class="card-title">${phoneData.phone_name}</h5>
                   <p class="card-text">${phoneData.brand}</p>
-                  <a onclick=loadPhoneDetails(${phoneData.slug})" href="#" class="btn btn-primary">Go somewhere</a>
+                  <a onclick="loadPhoneDetails('${phoneData.slug}')" href="#" class="btn btn-primary">Details</a>
                 </div>
             </div>
         `
@@ -37,26 +41,32 @@ const displayPhones = data =>{
 }
 //indivisual phone finding
 const loadPhoneDetails = phoneSlug =>{
-    // console.log(phoneSlug);
-    const url = `https://openapi.programming-hero.com/api/phone/${slug}`
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneSlug}`;
+    // console.log(url);
     fetch (url)
     .then (res => res.json())
-    .then (data => phoneDetailsDisplay(data.data[0]))
+    .then (data => phoneDetailsDisplay(data.data))
+    .catch(error => displayError(error));
+}
+// error function
+const displayError = error =>{
+    document.getElementById('error-massage').style.display = 'block';
 }
 
 const phoneDetailsDisplay = phoneData =>{
-    const phoneDetails = document.getElementById('phone-details')
-    phoneDetails.textContent = " ";
+    console.log(phoneData);
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = ' ';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
         <img src="${phoneData.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${phoneData.phone_name}</h5>
-            <p class="card-text">${phoneData.chipSet}</p>
-            <p class="card-text">${phoneData.memory}</p>
-            <p class="card-text">${phoneData.sensors}</p>
-            <p class="card-text">${phoneData.others}</p>
+            <h5 class="card-title"><b>Name:</b> ${phoneData.name}</h5>
+            <p class="card-text"><b>ChipSet:</b> ${phoneData.mainFeatures.chipSet}</p>
+            <p class="card-text"><b>Storage:</b> ${phoneData.mainFeatures.memory}</p>
+            <p class="card-text"><b>Sensors:</b> ${phoneData.mainFeatures.sensors}</p>
+            <p class="card-text"><b>Others:</b> ${phoneData.others}</p>
             
         </div>
     `
